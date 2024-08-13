c636d65462186af1bfa2ef848f604d95b367b035
Utkarsh Arora <116023139+Utkarsh5669@users.noreply.github.com>
2024-08-13 23:11:19 +0530

    Add files via upload

---
+++ Employee_Retention_System/Employee_Retention/apps/core/config.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+import random
+
+class Config:
+
+
+    def __init__(self):
+        self.training_data_path = 'data/training_data'
+        self.training_database = 'training'
+        self.prediction_data_path = 'data/prediction_data'
+        self.prediction_database = 'prediction'
+
+    def get_run_id(self):
+
+        self.now = datetime.now()
+        self.date = self.now.date()
+        self.current_time = self.now.strftime("%H%M%S")
+        return str(self.date)+"_"+str(self.current_time)+"_"+str(random.randint(100000000, 999999999))
